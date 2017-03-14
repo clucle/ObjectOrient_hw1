c8cc74209fffd0e6e9219e05bbe7cec3d5024f86
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-14 16:42:52 +0900

    feat: AddressBook base struct
    add functions, member variables

--- HomeWork1/AddressBook.h
+++ HomeWork1/AddressBook.h
@@ -1,8 +1,28 @@
 #pragma once
+#include "Person.h"
+
 class CAddressBook
 {
+private:
+    unsigned __int16 m_nCount;
+    CPerson* m_pPerson = NULL;
+
 public:
     CAddressBook();
     ~CAddressBook();
+
+    void AddPerson(string sName, string sNumber,
+        string sRelation);
+    void DelPerson(string sName = NULL,
+        string sPhone = NULL);
+
+    void AddRelation(string sName);
+    void DelRelation(string sName);
+
+    void Load();
+    void Save();
+
+    void Search();
+    void CallMenu();
 };
 
