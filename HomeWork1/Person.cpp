f3b22f364cde05bafc7563ce872beaed3e0f4820
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-15 16:50:43 +0900

    feat: update Person class add Email
    사람 클래스에 이메일 멤버변수 추가하고, 생성자, getter,setter 변경

--- HomeWork1/Person.cpp
+++ HomeWork1/Person.cpp
@@ -1,10 +1,11 @@
 #include "Person.h"
 
-CPerson::CPerson(string sName, string sNumber, string sRelation)
+CPerson::CPerson(string sName, string sNumber, string sRelation, string sEmail)
 {
     m_sName = sName;
     m_sNumber = sNumber;
     m_sRelation = sRelation;
+    m_sEmail = sEmail;
 }
 
 CPerson::~CPerson()
--- HomeWork1/Person.h
+++ HomeWork1/Person.h
@@ -9,14 +9,17 @@ private:
     string m_sName;
     string m_sNumber;
     string m_sRelation;
+    string m_sEmail;
 
 public:
-    CPerson(string sName, string sNumber, string m_sRelation);
+    CPerson(string sName, string sNumber,
+         string m_sRelation, string m_sEmail);
     ~CPerson();
 
     string getName() { return m_sName; }
     string getNumber() { return m_sNumber; }
     string getRelation() { return m_sRelation; }
+    string getEmail() { return m_sEmail; }
 
     string setName(string sName) 
         { m_sName = sName; }
@@ -24,5 +27,7 @@ public:
         { m_sNumber = sNumber; }
     string setRelation(string sRelation)
         { m_sRelation = sRelation; }
+    string setEmail(string sEmail)
+        { m_sEmail = sEmail; }
 };
 
