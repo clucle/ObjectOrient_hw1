4fb921936c15ab8d27ba6d306c66669518e4157f
RisingJade <csm48173738@gmail.com>
2017-03-15 17:53:40 +0900

    feat: Main -> add person 		이메일을 추가하여 이메일 관련 생성자 및 각종 함수에 		이메일변수 추가

--- HomeWork1/AddressBook.h
+++ HomeWork1/AddressBook.h
@@ -2,21 +2,20 @@
 #include "Person.h"
 #include <fstream>
 #include <iostream>
-
+#include <vector>
+using namespace std;
 class CAddressBook
 {
 private:
     unsigned __int16 m_nCount;
-    CPerson* m_pPerson = NULL;
+    vector<CPerson> m_pPerson;
 
 public:
     CAddressBook();
     ~CAddressBook();
 
-    void AddPerson(string sName, string sNumber,
-        string sRelation);
-    void DelPerson(string sName = NULL,
-        string sPhone = NULL);
+    void AddPerson(string sName, string sNumber, string sRelation, string sEmail);
+    void DelPerson(string sName = NULL, string sPhone = NULL);
 
     void AddRelation(string sName);
     void DelRelation(string sName);
--- HomeWork1/main.cpp
+++ HomeWork1/main.cpp
@@ -1,10 +1,21 @@
 #include "AddressBook.h"
-
+using namespace std;
 int main() {
     CAddressBook* pDevice = new CAddressBook();
+	
+	int state = 0;
+	while (1) {
+		printf("당신의 선택 0 나가기 1 사람 추가 : ");
+		scanf("%d", &state);
 
+		if (!state) break;
 
-
+	}
+		
+	for (int i =0;i<10;i++)
+	{
+		pDevice->AddPerson()
+	}
     delete pDevice;
     return 0;
 }
