0eeacc143ed362bfae0b9c17cb3b1c6ea0f78c92
clucle <wjdenwls123@gmail.com>
2017-03-18 16:49:17 +0900

    feat: add search class

---
+++ HomeWork1/Search.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <vector>
+
+class CSearch
+{
+public:
+    CSearch();
+    ~CSearch();
+
+    bool isSearched(std::string sName, std::string sFind);
+};
+
